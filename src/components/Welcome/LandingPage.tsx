db398f2a4eddbfef374ebbfd06d11a53dbd778d2
Eitan Fire <eitanfire@gmail.com>
2025-02-02 16:11:15 -0700

    resize image proportionally

--- src/components/Welcome/LandingPage.tsx
+++ src/components/Welcome/LandingPage.tsx
@@ -3,6 +3,7 @@ import picture from '../../assets/efire Background Removed.png';
 import EmailContact from '../EmailContact';
 import classes from './Welcome.module.css';
 
+
 export function LandingPage() {
   const { colorScheme } = useMantineColorScheme();
 
@@ -21,22 +22,24 @@ export function LandingPage() {
               <Card
                 padding="xl"
                 radius="md"
-                pl='0'
+                pl="0"
                 mt="40%"
                 withBorder
                 style={{
                   backgroundColor: colorScheme === 'dark' ? '#25262B' : 'white',
                 }}
               >
-                <Grid gutter="lg" align="center">
+                <Grid gutter="sm" align="center">
                   <Grid.Col span={6}>
                     <Center>
                       <Image
                         src={picture}
                         className="picture"
                         radius="90%"
-                        height={110}
+                        height={'auto'}
                         style={{
+                          maxWidth: '70%',
+                          minWidth: '65%',
                           backgroundColor: colorScheme === 'dark' ? '#ccfcf4' : ' #f2f2f2',
                         }}
                       />
@@ -45,7 +48,23 @@ export function LandingPage() {
                   <Grid.Col span={6}>
                     <Center>
                       <div style={{ width: '100%' }}>
-                        <Title ta="left" size="xl" mb="xs">
+                        <Title
+                          ta="left"
+                          size="30"
+                          mb="xs"
+                      sx={(theme) => ({
+    fontSize: '4.5vw', // Default
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      fontSize: '3.5vw', // Medium screens
+    },
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      fontSize: '2.5vw', // Small screens
+    },
+  })}
+  style={{
+    color: colorScheme === 'dark' ? 'pink' : 'black',
+  }}
+                        >
                           Eitan Fire
                         </Title>
                         <Text>builder + teacher</Text>
