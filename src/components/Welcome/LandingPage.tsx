a701e38449fcdb143aa2b26898b0c35b912e1b96
Eitan Fire <eitanfire@gmail.com>
2025-02-04 15:28:49 -0700

    stack elements of card on smaller screen size

--- src/components/Welcome/LandingPage.tsx
+++ src/components/Welcome/LandingPage.tsx
@@ -1,10 +1,10 @@
+import { useEffect, useState } from "react";
 import {
   Anchor,
   Card,
   Center,
   Container,
   Grid,
-  Group,
   Image,
   Paper,
   Text,
@@ -13,44 +13,61 @@ import {
 } from "@mantine/core";
 import picture from "../../assets/efire Background Removed.png";
 import EmailContact from "../EmailContact";
-import { QRCodeToggle } from "../QRCodeToggle"; // Import the new QRCodeToggle component
 import classes from "./Welcome.module.css";
 
 export function LandingPage() {
   const { colorScheme } = useMantineColorScheme();
-  const pageURL = "https://eitans.website/"; // Updated link
+  const pageURL = "https://eitans.website/";
+
+  function useMediaQuery(query: string) {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+      const listener = () => setMatches(media.matches);
+      media.addEventListener("change", listener);
+      return () => media.removeEventListener("change", listener);
+    }, [matches, query]);
+
+    return matches;
+  }
+
+  const isLargeScreen = useMediaQuery("(min-width: 768px)");
 
   return (
     <div
       style={{
         backgroundColor: colorScheme === "dark" ? "#1A1B1E" : "#f0f0f0",
-        minHeight: "90vh",
-        padding: "2rem 0",
+        minHeight: "100dvh",
+        padding: "1rem",
+        display: "flex",
+        alignItems: "center",
       }}
     >
-      <Container size="md">
+      <Container size="md" style={{ width: "100%" }}>
         <Grid justify="center">
-          <Grid.Col span={8}>
+          <Grid.Col span={{ sm: 10, md: 8 }}>
             <Paper className={classes.component}>
               <Card
-                padding="xl"
+                padding="lg"
                 radius="md"
-                pl="0"
-                mt="40%"
                 withBorder
                 style={{
                   backgroundColor: colorScheme === "dark" ? "#25262B" : "white",
                 }}
               >
                 <Grid gutter="sm" align="center">
-                  <Grid.Col span={6}>
+                  <Grid.Col span={{ base: 12, sm: 6 }}>
                     <Center>
                       <Image
                         src={picture}
                         className="picture"
                         radius="90%"
                         fit="contain"
-                        w="70%"
+                        w={{ base: "50%", sm: "70%" }}
                         styles={{
                           root: {
                             backgroundColor:
@@ -60,48 +77,73 @@ export function LandingPage() {
                       />
                     </Center>
                   </Grid.Col>
-                  <Grid.Col span={6}>
-                    <Center>
-                      <div style={{ width: "100%" }}>
-                        <Title
-                          ta="left"
-                          size="3dvw"
-                          mb="xs"
-                          style={{
-                            color: colorScheme === "dark" ? "pink" : "black",
-                          }}
-                        >
-                          Eitan Fire
-                        </Title>
-                        <Text>builder + teacher</Text>
+                  <Grid.Col span={{ base: 12, sm: 6 }}>
+                    <div
+                      style={{
+                        width: "100%",
+                        padding: "0.5rem",
+                      }}
+                    >
+                      <Title
+                        ta={{ base: "center", sm: "left" }}
+                        size="2.5rem"
+                        mb="xs"
+                        style={{
+                          color: colorScheme === "dark" ? "pink" : "black",
+                        }}
+                      >
+                        Eitan Fire
+                      </Title>
+                      <Text size="lg" ta={{ base: "center", sm: "left" }}>
+                        builder + teacher
+                      </Text>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: isLargeScreen
+                            ? "flex-start"
+                            : "center",
+                        }}
+                      >
                         <EmailContact />
-                        <Group justify="left" gap="md">
-                          {[
-                            {
-                              href: "https://github.com/eitanfire",
-                              label: "GitHub",
-                            },
-                            {
-                              href: "https://www.linkedin.com/in/eitanfire/",
-                              label: "LinkedIn",
-                            },
-                            {
-                              href: "https://projects.eitans.website/",
-                              label: "Projects",
-                            },
-                          ].map((link) => (
-                            <Anchor
-                              key={link.href}
-                              href={link.href}
-                              target="_blank"
-                              underline="hover"
-                            >
-                              {link.label}
-                            </Anchor>
-                          ))}
-                        </Group>
                       </div>
-                    </Center>
+                      <div
+                        style={{
+                          marginTop: "1rem",
+                          display: "flex",
+                          justifyContent: isLargeScreen
+                            ? "flex-start"
+                            : "center",
+                          gap: "1rem",
+                          flexWrap: "wrap",
+                        }}
+                      >
+                        {[
+                          {
+                            href: "https://github.com/eitanfire",
+                            label: "GitHub",
+                          },
+                          {
+                            href: "https://www.linkedin.com/in/eitanfire/",
+                            label: "LinkedIn",
+                          },
+                          {
+                            href: "https://projects.eitans.website/",
+                            label: "Projects",
+                          },
+                        ].map((link) => (
+                          <Anchor
+                            key={link.href}
+                            href={link.href}
+                            target="_blank"
+                            underline="hover"
+                            size="sm"
+                          >
+                            {link.label}
+                          </Anchor>
+                        ))}
+                      </div>
+                    </div>
                   </Grid.Col>
                 </Grid>
               </Card>
