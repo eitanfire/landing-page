495b33bab3e7c7c93cc4cba818f6a1ac48bd8ddf
Eitan Fire <eitanfire@gmail.com>
2025-02-04 12:14:43 -0700

    add qr code toggle

---
+++ src/components/QRCodeToggle.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import { Button, Center, Flex, Modal } from "@mantine/core";
+
+interface QRCodeToggleProps {
+  url: string;
+}
+
+export function QRCodeToggle({ url }: QRCodeToggleProps) {
+  const [showQR, setShowQR] = useState(false);
+
+  const handleCopyLink = () => {
+    navigator.clipboard.writeText(url);
+    alert("Link copied to clipboard!");
+  };
+
+  return (
+    <>
+      <Flex gap="md" align="center">
+        <Button onClick={() => setShowQR(true)}>Show QR Code</Button>
+        <Button variant="outline" onClick={handleCopyLink}>
+          Copy Link
+        </Button>
+      </Flex>
+
+      {/* Full-Screen QR Code Modal */}
+      <Modal
+        opened={showQR}
+        onClose={() => setShowQR(false)}
+        centered
+        fullScreen
+      >
+        <Center style={{ height: "100vh", flexDirection: "column" }}>
+          <QRCode value={url} size={250} />
+          <Button mt="md" onClick={() => setShowQR(false)}>
+            Close
+          </Button>
+        </Center>
+      </Modal>
+    </>
+  );
+}
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,17 +1,24 @@
-import { useMantineColorScheme } from '@mantine/core';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { LandingPage } from '../components/Welcome/LandingPage';
+import { useMantineColorScheme, Flex } from "@mantine/core";
+import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
+import { LandingPage } from "../components/Welcome/LandingPage";
+import { QRCodeToggle } from "../components/QRCodeToggle";
 
 export function HomePage() {
   const { colorScheme } = useMantineColorScheme();
+  const pageURL = "https://eitans.website/";
 
   return (
     <div
       style={{
-        backgroundColor: colorScheme === 'dark' ? '#1A1B1E' : '#f0f0f0',
+        backgroundColor: colorScheme === "dark" ? "#1A1B1E" : "#f0f0f0",
+        minHeight: "100vh",
       }}
     >
-      <ColorSchemeToggle />
+      <Flex justify="space-between" align="center" p="md">
+        <ColorSchemeToggle />
+        <QRCodeToggle url={pageURL} />
+      </Flex>
+
       <LandingPage />
     </div>
   );
