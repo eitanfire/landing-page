43e773b27efe9b780070092d9bbb695a2c0b438e
Eitan Fire <eitanfire@gmail.com>
2025-04-28 23:35:43 -0600

    delete unused import

--- src/components/BoulderJS.tsx
+++ src/components/BoulderJS.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Box, Button, Collapse, Center, Container } from "@mantine/core";
 import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 
