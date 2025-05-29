3bef6095808923d6d9e37cc82d58188215a33b94
Eitan Fire <eitanfire@gmail.com>
2025-05-28 22:21:50 -0600

    format

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,12 +1,12 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB_zNMrW-7o8VQ1IIB8dZQrLRSi32t5558',
-  authDomain: 'eitan-fires-landing-page.firebaseapp.com',
-  projectId: 'eitan-fires-landing-page',
-  storageBucket: 'eitan-fires-landing-page.firebasestorage.app',
-  messagingSenderId: '590332437169',
-  appId: '1:590332437169:web:0fc54fccba8bdb1d5e7890',
+  apiKey: "AIzaSyB_zNMrW-7o8VQ1IIB8dZQrLRSi32t5558",
+  authDomain: "eitan-fires-landing-page.firebaseapp.com",
+  projectId: "eitan-fires-landing-page",
+  storageBucket: "eitan-fires-landing-page.firebasestorage.app",
+  messagingSenderId: "590332437169",
+  appId: "1:590332437169:web:0fc54fccba8bdb1d5e7890",
 };
 
 const app = initializeApp(firebaseConfig);
